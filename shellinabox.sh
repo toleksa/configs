85212ad7e067b0b72455a74b86c9a27bde66fbcf
toleksa <toleksa@gmail.com>
2022-04-11 13:41:28 +0200

    restrict home

--- shellinabox.sh
+++ shellinabox.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 mkdir /home/shell/bin
+chmod 755 /home/shell/bin
 ln -s /usr/bin/ssh /.home/shell/bin/ssh
 
-echo "" > /home/shell/.bash_profile
+echo "export HISTFILE=/dev/null" > /home/shell/.bash_profile
 echo "PATH=$HOME/bin" > /home/shell/.bashrc
 chmod 444 /home/shell/.bash_profile /home/shell/.bashrc
 chattr +i /home/shell/.bashrc /home/shell/.bash_profile
+chmod 500 /home/shell
 
