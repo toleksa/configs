3a27fe89e20aad8ce8365c899530db27ab0b6f02
toleksa <toleksa@gmail.com>
2022-04-12 14:23:59 +0200

    fix

--- shellinabox.sh
+++ shellinabox.sh
@@ -5,7 +5,8 @@ chmod 755 /home/guest/bin
 ln -s /usr/bin/ssh /.home/guest/bin/ssh
 
 echo "export HISTFILE=/dev/null" > /home/guest/.bash_profile
-echo "export PATH=/home/guest/bin" > /home/guest/.bashrc
+echo "export PATH=/home/guest/bin" >> /home/guest/.bash_profile
+echo "" > /home/guest/.bashrc
 chmod 444 /home/guest/.bash_profile /home/guest/.bashrc
 chattr +i /home/guest/.bashrc /home/guest/.bash_profile
 chmod 500 /home/guest
