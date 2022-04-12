3903cb679d73e7bf8a4b2c1051c763cb2092a6b6
toleksa <toleksa@gmail.com>
2022-04-12 14:55:46 +0200

    fix, v1.0

--- shellinabox.sh
+++ shellinabox.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-rm -rf /home/guest/*
+rm -rf /home/guest/* /home/guest/.*
 echo "export HISTFILE=/dev/null" > /home/guest/.bash_profile
 echo "export PATH=/home/guest" >> /home/guest/.bash_profile
-chmod 444 /home/guest/.*
+chmod 555 /home/guest/.bash_profile
 chattr +i /home/guest/.*
 chmod 500 /home/guest
+chmod 555 /home
 ln -s /usr/bin/ssh /home/guest/ssh
 
