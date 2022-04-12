8e6f30c3cdd78602fe8348ee82735d224dfeb91e
toleksa <toleksa@gmail.com>
2022-04-12 14:27:24 +0200

    fix

--- shellinabox.sh
+++ shellinabox.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mkdir /home/guest/bin
-chmod 755 /home/guest/bin
-ln -s /usr/bin/ssh /.home/guest/bin/ssh
+#mkdir /home/guest/bin
+#chmod 755 /home/guest/bin
+ln -s /usr/bin/ssh /.home/guest/ssh
 
 echo "export HISTFILE=/dev/null" > /home/guest/.bash_profile
-echo "export PATH=/home/guest/bin" >> /home/guest/.bash_profile
+echo "export PATH=/home/guest" >> /home/guest/.bash_profile
 echo "" > /home/guest/.bashrc
 chmod 444 /home/guest/.bash_profile /home/guest/.bashrc
 chattr +i /home/guest/.bashrc /home/guest/.bash_profile
