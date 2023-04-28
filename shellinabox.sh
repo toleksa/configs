cedb032bb4ede79948fadee363accd3c43b352f5
toleksa <toleksa@gmail.com>
2023-04-28 17:41:18 +0200

    shellinabox.sh - adjust limits

--- shellinabox.sh
+++ shellinabox.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "shell hard nproc 4096" >> /etc/security/limits.conf
-echo "shell soft nproc 4096" >> /etc/security/limits.conf
-echo "shell hard nofile 512" >> /etc/security/limits.conf
-echo "shell soft nofile 512" >> /etc/security/limits.conf
+echo "shell hard nproc 256" >> /etc/security/limits.conf
+echo "shell soft nproc 256" >> /etc/security/limits.conf
+echo "shell hard nofile 256" >> /etc/security/limits.conf
+echo "shell soft nofile 256" >> /etc/security/limits.conf
 
 rm -rf /home/guest/* /home/guest/.*
 touch /home/guest/.hushlogin
